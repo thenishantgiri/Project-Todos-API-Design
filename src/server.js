80623b69387c143a9454d8c3ab1d854cdf32d5cf
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-20 14:59:10 +0530

    setup code & express

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/hello', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server listening on port http://localhost:3000')
+  })
+}
+
