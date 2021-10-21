d7a7edc478c7ef54aac1991a6d6775ee53f2edcf
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-21 21:37:04 +0530

    eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   env: {
     node: true,
   },
   extends: [
-    "standard",
-    "prettier",
-    "prettier/standard",
-    "plugin:jest/recommended",
+    'standard',
+    'prettier',
+    'prettier/standard',
+    'plugin:jest/recommended',
   ],
-  plugins: ["prettier", "jest"],
+  plugins: ['prettier', 'jest'],
   rules: {
-    "promise/catch-or-return": "error",
-    "prettier/prettier": [
-      "error",
+    'promise/catch-or-return': 'error',
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
         semi: false,
       },
     ],
   },
-};
+}
