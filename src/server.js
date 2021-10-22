892751a17594cd281f90023c0ed7c3158ad623ce
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-22 05:30:17 +0530

    implemented signup, signin & locked-down the api server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
+
 import { connect } from './utils/db'
+import { signup, signin, protect } from './utils/auth'
+
 import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
 import listRouter from './resources/list/list.router'
@@ -13,10 +15,18 @@ export const app = express()
 app.disable('x-powered-by')
 
 app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+// User Signup & Signin
+app.post('/signup', signup)
+app.post('/signin', signin)
+
+// Locking-down the API : must be implemented before Routing
+app.use('/api', protect)
+
+// Routers
 app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/list', listRouter)
