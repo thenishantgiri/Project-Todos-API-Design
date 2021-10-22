1fe62902afd32ed29effc5561e0bdfabfec18759
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-22 05:35:18 +0530

    controllers added to item.router

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,17 +1,16 @@
 import { Router } from 'express'
+import controllers from './item.controllers'
+
 const router = Router()
 
-const mockController = (req, res) => {
-  res.json({ message: 'ok' })
-}
 // /api/item
-router.route('/').get(mockController).post(mockController)
+router.route('/').get(controllers.getOne).post(controllers.createOne)
 
 // /api/item/:id
 router
   .route('/:id')
-  .get(mockController)
-  .put(mockController)
-  .delete(mockController)
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.removeOne)
 
 export default router
